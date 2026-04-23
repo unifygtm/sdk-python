67ca418c27d73525d1e8665b075632b6c238210d
Sam Waterbury <sam@unifygtm.com>
2026-04-22 23:16:39 -0700

    Add all UValue variants

--- src/unify/types/data/u_value.py
+++ src/unify/types/data/u_value.py
@@ -3,13 +3,21 @@
 from typing import Union
 from typing_extensions import TypeAlias
 
+from .u_url import UURL
 from .u_date import UDate
+from .u_text import UText
+from .u_uuid import UUuid
+from .u_email import UEmail
+from .u_select import USelect
 from .u_address import UAddress
 from .u_boolean import UBoolean
 from .u_country import UCountry
 from .u_decimal import UDecimal
+from .u_integer import UInteger
 from .u_currency import UCurrency
+from .u_datetime import UDatetime
 from .u_multiselect import UMultiselect
+from .u_phone_number import UPhoneNumber
 from .u_reference_by_id import UReferenceByID
 from .u_reference_by_match import UReferenceByMatch
 from .u_reference_by_upsert import UReferenceByUpsert
@@ -22,9 +30,17 @@ UValue: TypeAlias = Union[
     UCountry,
     UCurrency,
     UDate,
+    UDatetime,
     UDecimal,
+    UEmail,
+    UInteger,
     UMultiselect,
+    UPhoneNumber,
     UReferenceByID,
     UReferenceByMatch,
     UReferenceByUpsert,
+    USelect,
+    UText,
+    UURL,
+    UUuid,
 ]
